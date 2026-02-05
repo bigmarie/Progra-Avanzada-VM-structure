1d3f544d2bd66779f8cfd695fdca51462dc51925
Samsa (J) <dierick.p121@gmail.com>
2026-02-05 10:19:03 -0600

    Update firewall_hardening.sh

--- firewall_hardening.sh
+++ firewall_hardening.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+
+sudo ufw --force reset
+
 sudo ufw default deny incoming
+
 sudo ufw default allow outgoing
+
+
+# 3. Permitir SSH (Puerto 22) - Solo lo esencial
+
 sudo ufw allow 22/tcp
+
+
+# Necesario para que el Red Team ataque y el Blue Team monitoree
+
+sudo ufw allow 8080/tcp
+
+
+# 5. Permitir ICMP (Ping) para diagnóstico del laboratorio
+
+sudo ufw allow icmp
+
+
+# 6. Habilitar el firewall
+
 sudo ufw --force enable
+
+
+# 7. Mostrar estado final
+
 sudo ufw status verbose
