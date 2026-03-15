b07cd81a94f166a41f1187a8d5ed9e0df7d8247d
Samsa (J) <dierick.p121@gmail.com>
2026-03-15 16:51:43 -0600

    Update firewall_hardening.sh

--- blue_team/firewall_hardening.sh
+++ blue_team/firewall_hardening.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
+sudo ufw --force reset
+ 
+# Politicas por defecto
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-sudo ufw allow 22/tcp
+ 
+# Permitir SSH con limite anti brute-force
+sudo ufw limit 22/tcp
+ 
+# Bloquear puertos peligrosos
+sudo ufw deny 23    # Telnet
+sudo ufw deny 21    # FTP
+sudo ufw deny 3389  # RDP
+sudo ufw deny 445   # SMB
+ 
+# Activar y mostrar estado
 sudo ufw --force enable
 sudo ufw status verbose
